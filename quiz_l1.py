a8156e7185a501ddf601d06dcf21c3238afe7127
SumanBanikSB <suman33banik@gmail.com>
2021-12-09 00:11:58 +0530

    changed quiz level1

--- quiz_l1.py
+++ quiz_l1.py
@@ -43,26 +43,39 @@ image4 = pygame.image.load('images/d2v47czu.bmp')
 image5 = pygame.image.load('images/9xkwbec5.bmp')
 image6 = pygame.image.load('images/unknown.svg')
 quizword = pygame.image.load('images/9ury7n0e.bmp')
-image8 = pygame.image.load('images/2tgnfeqy.bmp')
-errouB = pygame.image.load('images/j6hg7f08.bmp')
-errouC = pygame.image.load('images/ggbbhntn.bmp')
-errouA = pygame.image.load('images/zvp3qkce.bmp')
-errouD = pygame.image.load('images/k3kgvdbh.bmp')
+image8 = pygame.image.load('images/Q1.png')
+image8 = pygame.transform.scale(image8,(450,350))
+wrong = pygame.image.load('images/wrong.png')
+
 image10 = pygame.image.load('images/a5qvh3y1.bmp')
 
 textA = pygame.image.load('images/776yuygt.bmp')
 textB = pygame.image.load('images/ty7r6ybd.bmp')
 textC = pygame.image.load('images/s7n2usr1.bmp')
 textD = pygame.image.load('images/hbtviltr.bmp')
-question2a = pygame.image.load('images/7l5gkrcd.bmp')
+question2a = pygame.image.load('images/Q2.png')
+question2a = pygame.transform.scale(question2a, (450,300))
 score = 0
-question1a = pygame.image.load('images/dcjncg80.bmp')
-question3 = pygame.image.load('images/drgqabgo.bmp')
-question3a = pygame.image.load('images/t59n07ll.bmp')
-question4 = pygame.image.load('images/aww4eyu1.bmp')
-question4a = pygame.image.load('images/pbts0z83.bmp')
-question5 = pygame.image.load('images/h89oga3n.bmp')
-question5a = pygame.image.load('images/uk7ukzpi.bmp')
+question1a = pygame.image.load('images/Q1.png')
+question1a = pygame.transform.scale(question1a,(450,300))
+question3 = pygame.image.load('images/Q3.png')
+question3 = pygame.transform.scale(question3,(450,300))
+
+question3a = pygame.image.load('images/Q3.png')
+question3a = pygame.transform.scale(question3a,(450,300))
+
+question4 = pygame.image.load('images/Q4.png')
+question4 = pygame.transform.scale(question4,(450,300))
+
+question4a = pygame.image.load('images/Q4.png')
+question4a = pygame.transform.scale(question4a,(450,300))
+
+question5 = pygame.image.load('images/Q4.png')
+question5 = pygame.transform.scale(question5,(450,300))
+
+question5a = pygame.image.load('images/Q4.png')
+question5a = pygame.transform.scale(question5a,(450,300))
+
 question6 = pygame.image.load('images/yleehx0i.bmp')
 question6a = pygame.image.load('images/p1t9n4fl.bmp')
 question7 = pygame.image.load('images/05nry4xm.bmp')
@@ -78,6 +91,10 @@ p7 = pygame.image.load('images/xksxotnm.bmp')
 p8 = pygame.image.load('images/cwzw0q0l.bmp')
 p0 = pygame.image.load('images/ifpsfqjs.bmp')
 p1 = pygame.image.load('images/87cp8jek.bmp')
+
+wrong = pygame.image.load('images/wrong.png')
+correct = pygame.image.load('images/correct.png')
+
 a = 120;
 b = 2;
 
@@ -88,7 +105,7 @@ numero = 1
 screen.blit(image3,(200,260))  
 pygame.display.flip()
 Not_Pressed = True
-def Question_Prompt(certa, imagemQ, pergunta, custom, right_side, height):
+def Question_Prompt(certa, imagemQ, custom, right_side, height):
     global score
     screen.fill( test_color )
     running = True
@@ -113,8 +130,8 @@ def Question_Prompt(certa, imagemQ, pergunta, custom, right_side, height):
     if custom == True:
         screen.blit(imagemQ,(right_side,height))
     elif custom == False:
-        screen.blit(imagemQ,(40,235))
-    screen.blit(pergunta,(50,10))
+        screen.blit(imagemQ,(40,10))
+    # screen.blit(pergunta,(50,10))
     if certa == 'A':
         correct = rectA
     elif certa == 'B':
@@ -146,16 +163,16 @@ def Question_Prompt(certa, imagemQ, pergunta, custom, right_side, height):
                     if event.button ==1:
                         screen.fill( test_color )
                         if certa == 'B':
-                            screen.blit(errouB, (56,160))
+                            screen.blit(wrong, (56,160))
                             pygame.display.flip()
                         elif certa == 'C':
-                            screen.blit(errouC,(56,160))
+                            screen.blit(wrong,(56,160))
                             pygame.display.flip()
                         elif certa == 'D':
-                            screen.blit(errouD, (56,160))
+                            screen.blit(wrong, (56,160))
                             pygame.display.flip()
                         elif certa == 'A':   
-                            screen.blit(errouA, (56,160))
+                            screen.blit(wrong, (56,160))
                             pygame.display.flip()                        
                         pygame.time.wait(1000)
                         running = False
@@ -164,17 +181,17 @@ def Question_Prompt(certa, imagemQ, pergunta, custom, right_side, height):
                     if event.button ==1:
                         screen.fill( test_color )
                         if certa == 'B':
-                            screen.blit(errouB, (56,160))
+                            screen.blit(wrong, (56,160))
                             pygame.display.flip()
                         elif certa == 'C':
-                            screen.blit(errouC,(56,160))
+                            screen.blit(wrong,(56,160))
                             pygame.display.flip()
                         elif certa == 'D':
-                            screen.blit(errouD, (56,160))
+                            screen.blit(wrong, (56,160))
                             pygame.display.flip()
                         elif certa == 'A':
                             print("certa A")
-                            screen.blit(errouA, (56,160))
+                            screen.blit(wrong, (56,160))
                             pygame.display.flip()                     
                         pygame.time.wait(1000)
                         running = False
@@ -183,17 +200,17 @@ def Question_Prompt(certa, imagemQ, pergunta, custom, right_side, height):
                     if event.button ==1:
                         screen.fill( test_color )
                         if certa == 'B':
-                            screen.blit(errouB, (56,160))
+                            screen.blit(wrong, (56,160))
                             pygame.display.flip()
                         elif certa == 'C':
-                            screen.blit(errouC,(56,160))
+                            screen.blit(wrong,(56,160))
                             pygame.display.flip()
                         elif certa == 'D':
-                            screen.blit(errouD, (56,160))
+                            screen.blit(wrong, (56,160))
                             pygame.display.flip()
                         elif certa == 'A': 
                             print("certa A")
-                            screen.blit(errouA, (56,160))
+                            screen.blit(wrong, (56,160))
                             pygame.display.flip()                       
                         pygame.time.wait(1000)
                         running = False
@@ -202,17 +219,17 @@ def Question_Prompt(certa, imagemQ, pergunta, custom, right_side, height):
                     if event.button ==1:
                         screen.fill( test_color )
                         if certa == 'B':
-                            screen.blit(errouB, (56,160))
+                            screen.blit(wrong, (56,160))
                             pygame.display.flip()
                         elif certa == 'C':
-                            screen.blit(errouC,(56,160))
+                            screen.blit(wrong,(56,160))
                             pygame.display.flip()
                         elif certa == 'D':
-                            screen.blit(errouD, (56,160))
+                            screen.blit(wrong, (56,160))
                             pygame.display.flip()
                         elif certa == 'A':   
                             print("certa A")
-                            screen.blit(errouA, (56,160)) 
+                            screen.blit(wrong, (56,160)) 
                             pygame.display.flip()                      
                         pygame.time.wait(1000 )
                         running = False
@@ -232,45 +249,46 @@ while Not_Pressed == True:
                 pygame.mixer.init()
                 pygame.mixer.music.load(music)
                 pygame.mixer.music.play(-1)
-                pygame.mixer.music.set_volume(0.5)
+                pygame.mixer.music.set_volume(0)
                 screen.blit(quizword, (215, 30))
                 screen.blit(image8,(56,160))
                 cube = pygame.Surface([250, 250])
                 cube.fill((0,25,153))
                 pygame.display.flip() 
                 pygame.time.wait(1000)
-                Question_Prompt('B',question1a, image8, False, 0,0)
+                Question_Prompt('B',question1a, False, 0,0)
                 print("Passou 1")
                 screen.fill( test_color )
                 screen.blit(quizword, (215, 30))
                 screen.blit(question2a, (56,160))
                 pygame.display.flip()
                 print("DisplayUpdated")
-                quizabc = pygame.image.load('images/tlcyuigv.bmp')
+                quizabc = pygame.image.load('images/Q2.png')
+                quizabc = pygame.transform.scale(quizabc, (430,300))
                 print("Esperando cooldown de 6 segundos")
                 pygame.time.wait(1000)
                 print("Cooldown passou")
-                Question_Prompt('C',quizabc, question2a, False, 0,0)
+                Question_Prompt('C',quizabc, False, 0,0)
                 print("passou 2")
                 screen.fill( test_color )
                 screen.blit(quizword, (215, 30))
-                screen.blit(question3, (56,160))
+                screen.blit(question3, (56,150))
                 pygame.display.flip()
                 print("du")
                 print("cooldown")
                 pygame.time.wait(1000)
                 print("passou")
-                Question_Prompt('A', question3a, question3, True, 50,170)
+                Question_Prompt('A', question3a, True, 50,20)
                 print("passou 3")
                 screen.fill( test_color )
                 screen.blit(quizword, (215, 30))
-                screen.blit(question4, (56, 160))
+                screen.blit(question4, (56, 150))
                 pygame.display.flip()
                 print('du')
                 print("cooldown")
                 pygame.time.wait(1000)
                 print("passou")
-                Question_Prompt('D', question4a, question4, True, 50,150)
+                Question_Prompt('D', question4a, True, 50,20)
                 print("Passou 4")
                 screen.fill( test_color )
                 screen.blit(quizword, (215,30))
@@ -280,38 +298,8 @@ while Not_Pressed == True:
                 print("cooldown")
                 pygame.time.wait(1000)
                 print("passou")
-                Question_Prompt('A', question5a, question5, True, 50,150)
-                print("Passou 5")
-                screen.fill( test_color )
-                screen.blit(quizword, (215,30))
-                screen.blit(question6,(56,160))
-                pygame.display.flip()
-                print("du")
-                print("Cooldown")
-                pygame.time.wait(1000)
-                print("passou")
-                Question_Prompt('C', question6a, question6, True, 50,180)
-                print("passou6")
-                screen.fill( test_color )
-                screen.blit(quizword, (215,30))
-                screen.blit(question7,(56,160))
-                pygame.display.flip()
-                print("du")
-                print("Cooldown")
-                pygame.time.wait(1000)
-                print("passou")
-                Question_Prompt('C', question7a, question7, True, 50,150)
-                print("passou 7")
-                screen.fill( test_color )
-                screen.blit(quizword, (215,30))
-                screen.blit(question8,(56,160))
-                pygame.display.flip()
-                print("du")
-                print("Cooldown")
-                pygame.time.wait(1000)
-                print("passou") 
-                Question_Prompt('A', question8a, question8, True, 50,150)
-                screen.fill( test_color )
+                Question_Prompt('A', question5a, True, 50,20)
+                
                 if score == 0:
                     screen.blit(p0,(56,160))
                     print("Voce foi mal! Voce esta com 0 pontos no total")
@@ -330,15 +318,7 @@ while Not_Pressed == True:
                 elif score == 5:
                     print("Voce tirou mais de 60%, parabens! Mas, acho que voce consegue mais!")
                     screen.blit(p5,(56,160))
-                elif score == 6:
-                    print("Voce tirou 7, isto equivale mais de 75%, parabens!!")
-                    screen.blit(p6,(56,160))
-                elif score == 7:
-                    print("Voce e um deus, conseguiu 7 quase total!")
-                    screen.blit(p7,(56,160))
-                elif score == 8:
-                    print("Wow, estou surpreendido, voce conseguiu a nota máxima! Parabéns!")
-                    screen.blit(p8,(56,160))
+           
                 pygame.display.flip()
                 pygame.time.wait(1000)
                 pygame.quit()
